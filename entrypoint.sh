7ccdaa32d6bed1da0bf0741489b32ef1111f4e57
chris warth <csw@wylabs.ai>
2021-03-09 16:51:31 -0800

    use full path to python script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-python main.py "${INPUT_CONSTRAINTSFILE}" "${INPUT_DATAFILE}" "${INPUT_EXPECT_FAILURE}"
+python /main.py "${INPUT_CONSTRAINTSFILE}" "${INPUT_DATAFILE}" "${INPUT_EXPECT_FAILURE}"
 
